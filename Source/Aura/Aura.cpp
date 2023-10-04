be3dea1fb30b2449ea04033eb7a58f98135717b2
Antek <antek.strzalkowski@gmail.com>
2023-10-04 10:51:13 +0200

    The Base Character Class

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright Autonomaton
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Autonomaton
+
+#pragma once
+
+#include "CoreMinimal.h"
+
