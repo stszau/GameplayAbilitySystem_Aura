e9e6a2ed6f2a68ab0a7f9f87246c592bb7730dca
Antek <antek.strzalkowski@gmail.com>
2023-10-07 22:40:41 +0200

    The Player State

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright Autonomaton
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -0,0 +1,18 @@
+// Copyright Autonomaton
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+public:
+	AAuraPlayerState();
+};
